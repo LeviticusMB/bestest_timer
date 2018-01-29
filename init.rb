7a290022706e79c8388927d664b6e1b1a3739b3c
Martin Blom <martin@blom.org>
2018-01-29 14:33:08 +0100

    Version 1.2.1.

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :bestest_timer do
   name 'Bestest Punch Clock for Redmine'
   author 'Martin Blom'
   description "This is an awesome punch clock/timer plugin for Redmine. It's great. It's the bestest."
-  version '1.2.0'
+  version '1.2.1'
   url 'https://github.com/LeviticusMB/bestest_timer'
   author_url 'https://github.com/LeviticusMB'
 end
